ca5071b0b0a1f4e4c390fa5f7ce50f5f2a0d3cb1
WahyuTomo14 <wahyuxd14@gmail.com>
2025-09-14 22:13:05 +0700

    buat action print

--- app/Filament/Resources/Transaksis/Tables/TransaksisTable.php
+++ app/Filament/Resources/Transaksis/Tables/TransaksisTable.php
@@ -2,16 +2,17 @@
 
 namespace App\Filament\Resources\Transaksis\Tables;
 
+use Filament\Tables\Table;
+use Filament\Actions\Action;
+use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
-use Filament\Actions\EditAction;
-use Filament\Actions\ForceDeleteBulkAction;
 use Filament\Actions\RestoreBulkAction;
-use Filament\Actions\ViewAction;
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\TrashedFilter;
-use Filament\Tables\Table;
+use Filament\Actions\ForceDeleteBulkAction;
 
 class TransaksisTable
 {
@@ -58,6 +59,11 @@ class TransaksisTable
             ])
             ->recordActions([
                 ViewAction::make(),
+                Action::make('print')
+                    ->label('Print PDF')
+                    ->icon('heroicon-o-printer')
+                    ->color('warning')
+                    ->url(fn ($record) => route('transaksi.print', $record), true),
                 EditAction::make(),
             ])
             ->toolbarActions([
