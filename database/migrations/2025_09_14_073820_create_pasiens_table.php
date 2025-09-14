47f4b01b83ddbc5af30488d1d3e755bd855cd415
WahyuTomo14 <wahyuxd14@gmail.com>
2025-09-14 14:41:21 +0700

    buat table pasien

---
+++ database/migrations/2025_09_14_073820_create_pasiens_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Traits\BaseModelSoftDeleteDefault;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    use BaseModelSoftDeleteDefault;
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pasien', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama', 128);
+            $table->text('alamat')->nullable();
+            $table->integer('umur');
+            $table->enum('jenis_kelamin', ['Laki-laki', 'Perempuan']);
+            $table->text('diagnosa')->nullable();
+            $this->base($table);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pasien');
+    }
+};
