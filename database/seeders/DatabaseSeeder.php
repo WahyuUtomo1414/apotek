a996d621d801b951b87fb8bde9ce4f5f817ce381
WahyuTomo14 <wahyuxd14@gmail.com>
2025-09-14 23:51:01 +0700

    buat seeder obat

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,12 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
-        UserSeeder::class;
-        // User::factory(10)->create();
-
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            UserSeeder::class,
+            ObatSeeder::class,
+        ]);
     }
 }
