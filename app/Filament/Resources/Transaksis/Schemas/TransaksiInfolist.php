19d4c1b19e87d926a38dc3a52500d451341fbf6a
WahyuTomo14 <wahyuxd14@gmail.com>
2025-09-14 21:40:22 +0700

    edit infolist trx

--- app/Filament/Resources/Transaksis/Schemas/TransaksiInfolist.php
+++ app/Filament/Resources/Transaksis/Schemas/TransaksiInfolist.php
@@ -3,9 +3,13 @@
 namespace App\Filament\Resources\Transaksis\Schemas;
 
 use App\Models\Transaksi;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Schemas\Components\Grid;
+use Filament\Schemas\Components\Section;
 use Filament\Infolists\Components\IconEntry;
 use Filament\Infolists\Components\TextEntry;
-use Filament\Schemas\Schema;
+use Filament\Infolists\Components\RepeatableEntry;
 
 class TransaksiInfolist
 {
@@ -13,33 +17,68 @@ class TransaksiInfolist
     {
         return $schema
             ->components([
-                TextEntry::make('id_pasien')
-                    ->numeric(),
+                TextEntry::make('pasien.nama')
+                    ->label('Nama Pasien'),
                 TextEntry::make('total_harga')
-                    ->numeric(),
-                TextEntry::make('metode_pembayaran'),
+                    ->label('Total Harga')
+                    ->prefix('Rp. '),
                 TextEntry::make('keterangan')
+                    ->label('Keterangan')
                     ->placeholder('-')
                     ->columnSpanFull(),
+                TextEntry::make('metode_pembayaran')
+                    ->label('Metode Pembayaran'),
                 IconEntry::make('active')
+                    ->label('Status pembayaran')
                     ->boolean(),
-                TextEntry::make('created_by')
-                    ->numeric(),
-                TextEntry::make('updated_by')
-                    ->numeric()
-                    ->placeholder('-'),
-                TextEntry::make('deleted_by')
-                    ->numeric()
-                    ->placeholder('-'),
-                TextEntry::make('created_at')
-                    ->dateTime()
-                    ->placeholder('-'),
-                TextEntry::make('updated_at')
-                    ->dateTime()
-                    ->placeholder('-'),
-                TextEntry::make('deleted_at')
-                    ->dateTime()
-                    ->visible(fn (Transaksi $record): bool => $record->trashed()),
+                
+                Section::make('Detail Transaksi')
+                    ->description('Daftar obat yang dibeli')
+                    ->icon(Heroicon::ShoppingBag)
+                    ->schema([
+                        RepeatableEntry::make('detailTransaksi')
+                            ->label('-')
+                            ->schema([
+                                TextEntry::make('obat.nama')
+                                    ->label('Nama Obat'),
+                                TextEntry::make('jenis_penjualan')
+                                    ->label('Jenis Penjualan'),
+                                TextEntry::make('jumlah_beli')
+                                    ->label('Jumlah'),
+                                TextEntry::make('harga_satuan')
+                                    ->label('Harga Satuan')
+                                    ->prefix('Rp. '),
+                                TextEntry::make('subtotal')
+                                    ->label('Subtotal')
+                                    ->prefix('Rp. '),
+                            ])
+                            ->columns(5),
+                    ])
+                    ->columns(1)
+                    ->columnSpanFull(),
+
+                Section::make('Informasi Pengguna')
+                ->schema([
+                    TextEntry::make('createdBy.name')
+                        ->label('Created By'),
+                    TextEntry::make('updatedBy.name')
+                        ->label("Updated by"),
+                    TextEntry::make('deletedBy.name')
+                        ->label("Deleted by"),
+                ])->columns(3)->columnSpanFull()->collapsible(),
+
+                Section::make('Timestamps')
+                    ->schema([
+                        Grid::make(2)
+                            ->schema([
+                                TextEntry::make('created_at')
+                                    ->label('Created At')
+                                    ->dateTime('d/m/Y H:i'),
+                                TextEntry::make('updated_at')
+                                    ->label('Last Updated')
+                                    ->dateTime('d/m/Y H:i'),
+                            ]),
+                    ])->columnSpanFull()->collapsible(),
             ]);
     }
 }
