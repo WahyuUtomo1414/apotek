c7ec8c68545a6de691a4b51fe8d4b160fba89431
WahyuTomo14 <wahyuxd14@gmail.com>
2025-09-14 14:47:06 +0700

    buat table transaksi

---
+++ app/Models/Transaksi.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\AuditedBySoftDelete;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Transaksi extends Model
+{
+    use HasFactory, Notifiable, AuditedBySoftDelete, SoftDeletes;
+    protected $table = 'transaksi';
+    protected $guarded = ['id'];
+
+    public function pasien()
+    {
+        return $this->belongsTo(Pasien::class, 'id_pasien');
+    }
+}
---
+++ database/migrations/2025_09_14_074134_create_transaksis_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Traits\BaseModelSoftDeleteDefault;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    use BaseModelSoftDeleteDefault;
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaksi', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_pasien')
+                ->constrained('pasien');
+            $table->double('total_harga')->default(0);
+            $table->string('metode_pembayaran', 32);
+            $table->text('keterangan')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaksi');
+    }
+};
